bed2e8f90c42313e1c447f2fd5012495c3c16072
jobira <jobira.ahmad@gmail.com>
2023-11-27 21:42:26 +0100

    Full of OOP actions were added within whole project

---
+++ lib/question.dart
@@ -0,0 +1,13 @@
+class Question {
+   late String questionText;
+   late bool questionAnswer;
+
+  // Constructor to initialize the fields
+
+  Question({required String q, required bool a}) {
+    questionText = q;
+    questionAnswer = a;
+  }
+}
+
+
